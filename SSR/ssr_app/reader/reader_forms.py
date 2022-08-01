38d3d5c946991ba455d464bc9d36e064510ec190
TRezendes <78620355+TRezendes@users.noreply.github.com>
2022-07-31 22:06:53 -0400

    Added copyright notices to all files

--- SSR/ssr_app/reader/reader_forms.py
+++ SSR/ssr_app/reader/reader_forms.py
@@ -1,4 +1,11 @@
-from SSR.ssr_app import db
+"""
+This file is part of Simple Summer Reading.
+
+Simple Summer Reading is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with Simple Summer Reading. If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from flask_wtf import FlaskForm
 from SSR.ssr_app.models import LibraryTbl, LoginInfoTbl, StaffTbl, UserInfoTbl, ZipTbl, getSchools, getStates
 from wtforms import BooleanField, DateField, DecimalField, EmailField, FloatField, FormField, HiddenField, IntegerField, RadioField, SelectField, StringField, SubmitField, TelField, TextAreaField
--- SSR/ssr_app/reader/reader_routes.py
+++ SSR/ssr_app/reader/reader_routes.py
@@ -1,3 +1,11 @@
+"""
+This file is part of Simple Summer Reading.
+
+Simple Summer Reading is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+Foobar is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with Simple Summer Reading. If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from flask import Blueprint, flash, render_template, redirect, session, url_for
 from SSR.ssr_app.models import ActivityTbl, CompActsTbl, LibraryTbl, LoginInfoTbl, PrizeTbl, ReaderTbl, ReaderProgressTbl, SchoolTbl, StaffTbl, UserInfoTbl, ZipTbl, PrizeLevelVw, ReaderAgeLevelVw, ReaderGoalVw
 from .reader_forms import DeleteReaderForm, ProgressForm, ReaderRegForm
